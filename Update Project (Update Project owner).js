aceb6e333cbd61527deb51342afed427ca5c832e
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2024-03-13 17:49:18 +0000

    Create Update Project (Update Project owner).js

---
+++ Update Project (Update Project owner).js
@@ -0,0 +1,28 @@
+//https://apidocs.teamwork.com/docs/teamwork/v1/projects/put-projects-id-json
+const myHeaders = new Headers();
+const userName = "email address or API KEY here";
+const password = "password";
+const siteName = "yourSiteName"
+const projectId = "projectIdHere"
+const userId = "userIdHere"
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Basic " + btoa(userName + ":" + password));
+
+
+const raw = JSON.stringify({
+  "project": {
+    "projectOwnerId": userId
+  }
+});
+
+const requestOptions = {
+  method: "PUT",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch("https://" + siteName + ".teamwork.com/projects/" + projectId + ".json?", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
