06cf13812cc6a1f6b5c0513dc6c85d25fea635e7
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2024-03-13 15:58:24 +0000

    Create Edit an active time reminder.js

---
+++ Edit an active time reminder.js
@@ -0,0 +1,28 @@
+const myHeaders = new Headers();
+const userName = "email address or API KEY here";
+const password = "password";
+const siteName = "yourSiteName"
+const userId = "userIdHere"
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Basic "+btoa(userName+":"+password));
+
+const raw = JSON.stringify({
+  "reminder": {
+    "threshold": 60,//tracked less than ??% of user capacity
+    "time": "18:00:00",
+    "frequencyRRule": "FREQ=DAILY",
+    "isActive": true//Set to false to deactivate reminder
+  }
+});
+
+const requestOptions = {
+  method: "PATCH",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch("https://"+siteName+".teamwork.com/projects/api/v3/time/reminder.json", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
