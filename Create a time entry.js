1f1413f4845f27506eeae1856e35538e779e03a7
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2024-03-13 16:06:30 +0000

    Create Create a time entry.js

---
+++ Create a time entry.js
@@ -0,0 +1,32 @@
+const myHeaders = new Headers();
+const userName = "email address or API KEY here";
+const password = "password";
+const siteName = "yourSiteName"
+const taskId = "userIdHere"
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Basic "+btoa(userName+":"+password));
+
+const raw = JSON.stringify({
+  "time-entry": {
+    "description": "Time created via API",
+    "date": "20230801",//YYYYMMDD
+    "person-id": 256492,
+    "time": "15:00",
+    "hours": 4,
+    "minutes": 35,
+    "isbillable": true,//Set to false to make time entry non billable
+    "tags": "API"//Remove this line if you do not want to add a tag
+  }
+});
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch("https://"+siteName+".teamwork.com/tasks/"+taskId+"/time_entries.json", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
