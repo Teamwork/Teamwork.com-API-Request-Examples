b7baafbe3f754e68d222f53b5d4e7529fa4e3344
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2024-03-20 15:22:00 +0000

    Create Create a task.js

---
+++ postRequests /tasks/Create a task.js
@@ -0,0 +1,35 @@
+const myHeaders = new Headers();
+const userName = "email address or API KEY here";
+const password = "password";
+const siteName = "yourSiteName"
+const taskListId = taskListIdHere//integer
+const userId = userIdHere//integer
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Basic "+btoa(userName+":"+password));
+
+const raw = JSON.stringify({
+  "task": {
+    "tasklistId": taskListId,
+    "name": "Creating a task from the Teamwork.com API",
+    "assignees": {
+      "userIds": [
+        userId
+      ]
+    },
+    "startAt": "2024-04-16",
+    "dueAt": "2024-04-24",
+    "description": "[Create a task endpoint](https://apidocs.teamwork.com/docs/teamwork/v3/tasks/post-projects-api-v3-tasklists-tasklist-id-tasks-json)"
+  }
+});
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch("https://"+siteName+".teamwork.com/projects/api/v3/tasklists/"+taskListId+"/tasks.json", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
