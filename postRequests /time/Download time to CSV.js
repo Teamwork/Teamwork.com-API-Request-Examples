3590c529221835744d3c577cdfc59f4586e8a8cc
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2024-05-23 11:27:48 +0100

    Download time for a project to CSV.js

---
+++ postRequests /time/Download time to CSV.js
@@ -0,0 +1,38 @@
+const myHeaders = new Headers();
+const installId = "installIdHere";
+const userId = "userIdHere";
+const tw_key = tw_keyHere;
+const urlencoded = new URLSearchParams();
+urlencoded.append("action", "Time_DownloadReport");
+urlencoded.append("reportType", "csv");
+urlencoded.append("startDate", "20240301");
+urlencoded.append("endDate", "20240531");
+urlencoded.append("projectId", "732893");
+urlencoded.append("userId", "");
+urlencoded.append("teamIds", "");
+urlencoded.append("invoicedType", "all");
+urlencoded.append("billableType", "all");
+urlencoded.append("sortBy", "date");
+urlencoded.append("sortOrder", "asc");
+urlencoded.append("tagIds", "");
+urlencoded.append("companyId", "0");
+urlencoded.append("taskTagIds", "");
+urlencoded.append("matchAllTags", "true");
+urlencoded.append("projectsFromCompanyId", "0");
+urlencoded.append("projectStatus", "");
+urlencoded.append("onlyStarredProjects", "false");
+urlencoded.append("includeArchivedProjects", "false");
+urlencoded.append("selectedColumns", "date,project,whoLoggedTime,descriptionAndTags,attachedTaskList,startTime,endTime,isEntryBillable,hasEntryBeenBilled,timeTaken,hoursTaken,estimatedTime");
+urlencoded.append("includeTags", "true");
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: urlencoded,
+  redirect: "follow"
+};
+
+fetch("https://tw-export.teamwork.com/?tw_i="+installId+"&tw_u="+userId+"&tw_key="+tw_key, requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
