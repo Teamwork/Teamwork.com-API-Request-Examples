07dbd68f44bac5ac7a925382dd35f51824ca0d0f
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2024-09-02 00:07:45 +0100

    Update Get endpoint response headers.js

--- Get endpoint response headers.js
+++ Get endpoint response headers.js
@@ -1,6 +1,7 @@
 // This code sample is an example of some of the response headers data that is available with the V2 time entries endpoint
 // Response filtered to a date range - format of dates: yyyymmdd
-// Endpoint Url: https://" + siteName + ".teamwork.com/projects/api/v2/time.json
+// V1 Endpoint: https://" + siteName + ".teamwork.com/time_entries.json - https://apidocs.teamwork.com/docs/teamwork/v1/time-tracking/get-time-entries-json
+// V2 Endpoint: https://" + siteName + ".teamwork.com/projects/api/v2/time.json - Undocumented
 const myHeaders = new Headers();
 const userName = "email address or API KEY here";
 const password = "password";
@@ -17,15 +18,19 @@ const requestOptions = {
 };
 
 async function fetcHeaders() {
-    let latestActivityUrl = "https://" + siteName + ".teamwork.com/projects/api/v2/time.json?page=" + page + "&pageSize=50&getTotals=true&projectId=&companyId=0&userId=&assignedTeamIds=&invoicedType=all&billableType=all&fromDate=20240701&toDate=20240731&sortBy=date&sortOrder=asc&onlyStarredProjects=false&includeArchivedProjects=false&matchAllTags=true&projectStatus=all"
+    //let v1TimeUrl = "https://" + siteName + ".teamwork.com/time_entries.json?page=" + page + "&pageSize=50&getTotals=true&projectId=&companyId=0&userId=&assignedTeamIds=&invoicedType=all&billableType=all&fromDate=20240101&toDate=20240731&sortBy=date&sortOrder=asc&onlyStarredProjects=false&includeArchivedProjects=true&matchAllTags=true&projectStatus=all";
+    let v2TimeUrl = "https://" + siteName + ".teamwork.com/projects/api/v2/time.json?page=" + page + "&pageSize=50&getTotals=true&projectId=&companyId=0&userId=&assignedTeamIds=&invoicedType=all&billableType=all&fromDate=20240101&toDate=20240731&sortBy=date&sortOrder=asc&onlyStarredProjects=false&includeArchivedProjects=true&matchAllTags=true&projectStatus=all";
+    
     const response = await fetch(latestActivityUrl, requestOptions)
     let data = await response.json()
+    
     //console.log(response.headers) // Remove commenting at the start of this console log to get a full list of response headers for the endpoint
     console.log("\nHeaders - Page and rate limit info")
     console.log("Total records for request: " + response.headers.get('x-records'))
     console.log("Total pages: " + response.headers.get('x-pages'))
     console.log("Current page: " + response.headers.get('x-page'))
-    console.log("Total records on current page: " + data.timeEntries.length)
+    //console.log("Total records on current page (V1): " + data["time-entries"].length)
+    console.log("Total records on current page (V2): " + data.timeEntries.length)
     console.log("x-ratelimit-limit: " + response.headers.get('x-ratelimit-limit'))
     console.log("x-ratelimit-remaining: " + response.headers.get('x-ratelimit-remaining'))
 
