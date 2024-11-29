1d0996bbb5344fcf4b35fe6f1edfe472343ed6b8
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2024-11-29 12:36:58 +0000

    Create Get response headers for V1 and V3 endpoints.js

---
+++ Get response headers for V1 and V3 endpoints.js
@@ -0,0 +1,34 @@
+const myHeaders = new Headers();
+const userName = "email address or API KEY here";
+const password = "password";
+const siteName = "yourSiteName";
+
+let page = 1
+
+myHeaders.append("Authorization", "Basic " + btoa(userName + ":" + password));
+
+const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow"
+};
+
+async function fetcHeaders() {
+    let v1TasksUrl = "https://" + siteName + ".teamwork.com/tasks.json?page=" + page + "&pageSize=250";
+    let v3TasksUrl = "https://" + siteName + ".teamwork.com/projects/api/v3/tasks.json?page=" + page + "&pageSize=500";
+    const response = await fetch(v3TasksUrl, requestOptions)
+    let data = await response.json()
+    console.log(data)
+    console.log(response.headers)
+    console.log("\nHeaders - V1 Page info")
+    console.log("Total records for request: " + response.headers.get('x-records'))
+    console.log("Total pages: " + response.headers.get('x-pages'))
+    console.log("Current page: " + response.headers.get('x-page'))
+    /* console.log("\nHeaders - V3 Page info")
+    console.log(`Total records up to current page: ${data.meta.page.count}`) // This is built based on the following (pageSize*page+1 if hasMore = true) or (pageSize*page and or (part thereof on last page) if hasMore = false)
+    console.log(`Page offset: ${data.meta.page.pageOffset} - Page(${data.meta.page.pageOffset+1})`) // Page offset starts at 0 for page 1, pageoffset 1 for page 2, etc
+    console.log(`Page Size: ${data.meta.page.pageSize}`)
+    console.log(`Has More: ${data.meta.page.hasMore}`) */
+}
+
+fetcHeaders();
