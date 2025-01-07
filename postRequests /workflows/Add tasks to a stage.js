68d69e804ce86f9347a778a91ca83f5d52b77024
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2025-01-07 14:45:19 +0000

    Create Add tasks to a stage.js

---
+++ postRequests /workflows/Add tasks to a stage.js
@@ -0,0 +1,25 @@
+const myHeaders = new Headers();
+const userName = "email address or API KEY here";
+const password = "password";
+const siteName = "yourSiteName"
+const taskIds = [taskIdsHere] // array of integers - comma separated
+const stageId = stageIdHere; // integer
+const workflowId = workflowIdHere; // integer
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Basic "+btoa(userName+":"+password));
+
+const raw = JSON.stringify({
+  "taskIds": taskIds
+});
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch(`https://${siteName}.teamwork.com/projects/api/v3/workflows/${workflowId}/stages/${stageId}/tasks.json`, requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
