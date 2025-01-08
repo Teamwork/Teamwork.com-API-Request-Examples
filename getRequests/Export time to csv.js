5322cd6c7b252d1c78934bc475b3fd7d1e57c6bf
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2025-01-08 13:12:24 +0000

    Create Export time to csv.js

---
+++ getRequests/Export time to csv.js
@@ -0,0 +1,18 @@
+const myHeaders = new Headers();
+const userName = "email address or API KEY here";
+const password = "password";
+const siteName = "yourSiteName"
+
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", "Basic "+btoa(userName+":"+password));
+
+const requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow"
+};
+
+fetch(`https://${siteName}.teamwork.com/projects/api/v3/time.csv`, requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
