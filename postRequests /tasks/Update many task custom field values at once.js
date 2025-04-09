98ea44d2ddfc488046d883cf1ec8e90c759b0786
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2025-04-09 12:19:58 +0100

    Update Update many task custom field values at once.js

--- postRequests /tasks/Update many task custom field values at once.js
+++ postRequests /tasks/Update many task custom field values at once.js
@@ -11,27 +11,27 @@ const raw = JSON.stringify({
     "customfieldTasks": [
         {
             //sample Text based Field
-            "id": 86074,//customfieldValueId
-            "customfieldId": 2794,//customfieldId
+            "id": 86074,// This ID is for the value of the custom field which is unique to this task only
+            "customfieldId": 2794,// This ID is for the custom field entity which is a site wide id
             "value": "Value edited by API"
             //sample Text based Field
         },
         {
             //sample Dropdown based Field
-            "id": 86073,//customfieldValueId
-            "customfieldId": 2558,//customfieldId
+            "id": 86073,// This ID is for the value of the custom field which is unique to this task only
+            "customfieldId": 2558,// This ID is for the custom field entity which is a site wide id
             "value": "Submitted"
         }, 
         {
             //sample Integer based Field
-            "id": 86075,
-            "customFieldId": 2740,
+            "id": 86075,// This ID is for the value of the custom field which is unique to this task only
+            "customFieldId": 2740,// This ID is for the custom field entity which is a site wide id
             "value": 897
         },
         {
             //sample Date based Field
-            "id": 86076,
-            "customFieldId": 2739,
+            "id": 86076,// This ID is for the value of the custom field which is unique to this task only
+            "customFieldId": 2739,// This ID is for the custom field entity which is a site wide id
             "value": "2024-05-13"//YYYY-MM-DD
         }
     ]
