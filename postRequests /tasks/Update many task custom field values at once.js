5e32f11d9cf3c80e1859ebc775557467c0707753
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2025-04-09 12:57:49 +0100

    Update Update many task custom field values at once.js

--- postRequests /tasks/Update many task custom field values at once.js
+++ postRequests /tasks/Update many task custom field values at once.js
@@ -1,11 +1,17 @@
-//https://apidocs.teamwork.com/docs/teamwork/v3/custom-fields/post-projects-api-v3-tasks-task-id-customfields-bulk-update-json
+// This script will update all of the custom fields in the payload based on the custom field and custom field value id's on the specified task
+// Endpoint Documentation - https://apidocs.teamwork.com/docs/teamwork/v3/custom-fields/post-projects-api-v3-tasks-task-id-customfields-bulk-update-json
+// Supporting endpoint documantation:
+// - Get all customfields for tasks - https://apidocs.teamwork.com/docs/teamwork/v3/custom-fields/get-projects-api-v3-customfields-json > Make sure you add the entities param to filter task custom fields only
+//     - ie: {Domain}/projects/api/v3/customfields.json?page=1&pageSize=50&entities=task
+// - Task custom field values - https://apidocs.teamwork.com/docs/teamwork/v3/custom-fields/get-projects-api-v3-tasks-task-id-customfields-json > this endpoint can be used to get the id for the value of custom fields populates for a task
+
 const myHeaders = new Headers();
 const userName = "email address or API KEY here";
 const password = "password";
 const siteName = "yourSiteName"
 const taskId = "taskIdHere"
 myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Authorization", "Basic " + btoa(userName + ":" + password));
+myHeaders.append("Authorization", Basic " + btoa(userName + ":" + password));
 
 const raw = JSON.stringify({
     "customfieldTasks": [
@@ -44,7 +50,7 @@ const requestOptions = {
     redirect: "follow"
 };
 
-fetch("https://" + siteName + ".teamwork.com/projects/api/v3/tasks/" + taskId + "/customfields/bulk/update.json", requestOptions)
+fetch(`https://${siteName}.teamwork.com/projects/api/v3/tasks/${taskId}/customfields/bulk/update.json`, requestOptions)
     .then((response) => response.text())
     .then((result) => console.log(result))
     .catch((error) => console.error(error));
