83dd0caf9dc218d0cc8f823bbb64901e686d4935
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2025-09-11 11:00:27 +0100

    Update Create a task ( V3 endpoint ) with custom fields.js
    More objects added to add the majority of data

--- postRequests /tasks/Create a task ( V3 endpoint ) with custom fields.js
+++ postRequests /tasks/Create a task ( V3 endpoint ) with custom fields.js
@@ -1,51 +1,73 @@
+// This code sample will create a task with the following infiormation
+// > assign to a user or users if more are added 
+// > notify user(s) 
+// > put task on a workflow stage
+// > add tags
+// Endpoint documentation: https://apidocs.teamwork.com/docs/teamwork/v3/tasks/post-projects-api-v3-tasklists-tasklist-id-tasks-json
 const myHeaders = new Headers();
 const userName = "email address or API KEY here";
 const password = "password";
 const siteName = "yourSiteName"
-const userIds = "userIdsHere"
-const taskListId = "tasklistIdHere" // Required field
-const userId = "757564" // Comma separate additional user Ids if required - Leave blank if no-one is to be notified
-const columnId = "columnIdHere"// Leave blank if you are not setting a column for the task
+const userId = [12345] // Comma separate additional user Ids if required - Leave blank if no-one is to be assigned
+const commentFollowerId = [67890, 45678]; // Comma separate comment follower user Ids if required - Leave blank if no-one is to be added as a follower
+const changeFollowerId = [45678]; // Comma separate change follower user Ids if required - Leave blank if no-one is to be added as a follower
+const tagIds = [453455,767657];// Comma separate tagIds if required - Leave blank no tags are required
+const stageId = 54321; // integer - Leave blank if you are not setting a stage for the task
+const workflowId = 98765; // integer - Leave blank if you are not setting a stage for the task
 myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Authorization", "Basic "+btoa(userName+":"+password));
+myHeaders.append("Authorization", "Basic " + btoa(userName + ":" + password));
 
 const raw = JSON.stringify({
   "task": {
     "tasklistId": taskListId,
     "name": "Creating a task from the Teamwork.com API",
+    "tagIds": tagIds,
     "assignees": {
       "userIds": [
         userId
-      ]
+      ],
+      "teamIds": [],
+      "companyIds": []
+    },
+    "changeFollowers": {
+      "userIds": [
+        changeFollowerId
+      ],
+      "teamIds": [],
+      "companyIds": []
+    },
+    "commentFollowers": {
+      "userIds":
+        commentFollowerId
+      ,
+      "teamIds": [],
+      "companyIds": []
     },
     "priority": "medium",
     "startAt": "2024-04-16",
     "dueAt": "2024-04-24",
-    "tagIds": [
-            126792,
-            126786
-        ],
-    "estimatedMinutes": 420, // This must be an integer and related to total estimated minutes ie: 420 would be 7 hours
     "description": "This task was created with the V3 [Create a task endpoint](https://apidocs.teamwork.com/docs/teamwork/v3/tasks/post-projects-api-v3-tasklists-tasklist-id-tasks-json)",
     "customFields": [
-        {
-          "customFieldId": 2794,
-          "value": "QR5654"
-        },
-        {
-          "customFieldId": 3854,
-          "value": "https://apidocs.teamwork.com/docs/teamwork",
-          "urlTextToDisplay": "Teamwork.com API"
-        }
-      ]
+      {
+        "customFieldId": 2794,
+        "value": "QR5654"
+      },
+      {
+        "customFieldId": 6050,
+        "value": "https://apidocs.teamwork.com/docs/teamwork",
+        "urlTextToDisplay": "Teamwork.com API"
+      }
+    ]// comment this object out if you are not adding any custom field values
   },
-  "taskOptions" :{
-      "notify": true
+  "taskOptions": {
+    "notify": true
   },
-  "card": {
-      "columnId": columnId
-  },//Remove this object if you are not setting a column
-  
+  "workflows": {
+    "positionAfterTask": 0, // 0 to add the task to the bottom of the column, -1 to add the task to the top of the column and :taskId so that the task goes under the specified id
+    "stageId": stageId,
+    "workflowId": workflowId
+  },// Remove this object if you are not setting a workflow stage
+
 });
 
 const requestOptions = {
@@ -55,7 +77,7 @@ const requestOptions = {
   redirect: "follow"
 };
 
-fetch("https://"+siteName+".teamwork.com/projects/api/v3/tasklists/"+taskListId+"/tasks.json", requestOptions)
+fetch("https://" + siteName + ".teamwork.com/projects/api/v3/tasklists/" + taskListId + "/tasks.json", requestOptions)
   .then((response) => response.text())
   .then((result) => console.log(result))
   .catch((error) => console.error(error));
