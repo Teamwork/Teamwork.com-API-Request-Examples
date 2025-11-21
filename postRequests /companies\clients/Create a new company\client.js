3ea8b4b35befb3d12cfcf7b5627d8e30f7bdfb59
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2025-11-21 15:28:32 +0000

    Update Create a new company\client.js
    Reference to adding a currency for clients when the site is set up for multi currency. Disclaimer set to warn about the multi currency feature not being reversible once enabled on a site.

--- postRequests /companies\clients/Create a new company\client.js
+++ postRequests /companies\clients/Create a new company\client.js
@@ -1,6 +1,8 @@
 // This code sample is an example for creating a new client / company on Teamwork.com. 
 // Endpoint Documentation: https://apidocs.teamwork.com/docs/teamwork/v3/companies/post-projects-api-v3-companies-json
 // Associated guide: If you require a logo to be added for your new customer / client then please review https://apidocs.teamwork.com/guides/teamwork/file-uploading-via-the-api-preferred to get a file reference number
+// IMPORTANT: THE MULTI CURRENCY FEATURE IF ENABLED ON YOUR SITE WILL REQUIRE YOU TO ADD A CURRENCY VALUE FOR EACH NEW CLIENT. BE AWARE THAT THIS FEATURE CAN NOT BE REVERSED SO PLEASE READ https://support.teamwork.com/projects/finance/multi-currency#before-you-start
+// - If you require a list of currency Id's please use the following endpoint: GET: https://${siteName}.teamwork.com/projects/api/v3/currencies.json - note that this enpoint is undocumented but used in app for this purpose
 // Endpoint Url: https://${siteName}.teamwork.com/projects/api/v3/companies.json
 const myHeaders = new Headers();
 const userName = "email address or API KEY here";
@@ -20,8 +22,9 @@ const raw = JSON.stringify({
     "city": "Blackpool",
     "state": "Cork",
     "zip": "T23 F902",
-    "privateNotes": "Private notes go here\n",
-    "profile": "Public facing notes (in Teamwork.com) go here\n"
+    "privateNotes": "Private notes go here",
+    "profile": "Public facing notes (in Teamwork.com) go here"/* ,
+     "currencyId": 3 // ie: US dollar (USD) = 1, Canadian dollar (CAD) = 2, Euro (EUR) = 3, Pound sterling (GBP) = 4"*/
     //"logoPendingFileRef": ""// This process involves a getting reference number for a new file (tf_481bfbf0-094d-4d29-8dc7-1e765c8bcb8e.png) captured while following the upload a file via API process 
   }
 });
