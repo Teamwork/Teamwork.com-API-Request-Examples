155505f19e27a8a081821f7350fb927db82991af
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2026-02-04 13:29:15 +0000

    Update Create a time entry linked to a task (V3).js
    Required field flags added

--- postRequests /time/Create a time entry linked to a task (V3).js
+++ postRequests /time/Create a time entry linked to a task (V3).js
@@ -1,15 +1,15 @@
 // this code sample will log time on a task based on the task id provided
 // https://apidocs.teamwork.com/docs/teamwork/v3/time-tracking/post-projects-api-v3-tasks-task-id-time-json
-// REQUIRED FIELDS: date, time, minutes and userId
+// REQUIRED FIELDS: task id in the URL, date, time, minutes and userId
 const myHeaders = new Headers();
 const userName = "email address or API KEY here";
 const password = "password";
 const siteName = "yourSiteName";
 const projectId = projectIdHere; // Int
-const taskId = taskIdHere; // Int - Add a 0 for taskId if you want to log time at project level only
-const userId = userIdHere; // Int
-const date = "dateHere"; // ie: "2024-01-23" - Format: "yyyy-mm-dd"
-const time = "timeHere"; // ie: "17:18:00" - Format: "hh:mm:ss"
+const taskId = taskIdHere; // Required field Int - Add a 0 for taskId if you want to log time at project level only
+const userId = userIdHere; // Required field Int
+const date = "dateHere"; // Required field ie: "2024-01-23" - Format: "yyyy-mm-dd"
+const time = "timeHere"; // Required field ie: "17:18:00" - Format: "hh:mm:ss"
 myHeaders.append("Content-Type", "application/json");
 myHeaders.append("Authorization", "Basic "+btoa(userName+":"+password));
 
@@ -47,7 +47,7 @@ const requestOptions = {
   redirect: "follow"
 };
 
-fetch("https://"+siteName+".teamwork.com/projects/api/v3/tasks/"+taskId+"/time.json", requestOptions)
+fetch(`https://${siteName}.teamwork.com/projects/api/v3/tasks/${taskId}/time.json`, requestOptions)
   .then((response) => response.text())
   .then((result) => console.log(result))
   .catch((error) => console.error(error));
