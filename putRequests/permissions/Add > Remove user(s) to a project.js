07723b564effa1774aebe1c0aa8e168e9be6b068
Marc-Cash <84880775+Marc-Cash@users.noreply.github.com>
2026-09-25 14:01:45 +0100

    Create Add > Remove user(s) to a project.js

---
+++ putRequests/permissions/Add > Remove user(s) to a project.js
@@ -0,0 +1,30 @@
+// This code sample will add and or remove users from the specified project based on the ids passed into each object
+// Endpoint: https://apidocs.teamwork.com/docs/teamwork/v1/projects/put-projects-id-json
+const myHeaders = new Headers();
+const userName = "email address or API KEY here";
+const password = "password";
+const siteName = "yourSiteName"
+const addUserIds = "userIdsHere" // Comma separate for multiple users
+const removeUserIds = "userIdsHere" // Comma separate for multiple users
+const projectId = "ProjectIdHere"
+
+const raw = JSON.stringify({
+  "add": {
+    "userIdList": addUserIds
+  },
+  "remove": {
+    "userIdList": removeUserIds
+  }
+});
+
+const requestOptions = {
+  method: "PUT",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+
+fetch(`https://${siteName}/projects/${projectId}/people.json`, requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
